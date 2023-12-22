7b5cdab8718a7411f82ecf69b84e2ed22735ffe3
Raul Chimal <raulchimalhdz@hotmail.com>
2023-12-22 11:56:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, flash, redirect, render_template, request, session, url_for
-#from flask_session import Session
 from functools import wraps
+import os  # Don't forget to import os
 
 # Configure application
 app = Flask(__name__)
@@ -8,3 +8,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int
